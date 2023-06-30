345acccd300ab9fc9771451ad4712d19218757b5
Haqqi <haqqisetiadjie@student.uns.ac.id>
2023-07-01 02:47:25 +0700

    logaritma fix

--- logaritma_revisi.py
+++ logaritma_revisi.py
@@ -1,7 +1,7 @@
 # Mendeklarasikan 2 track kosong
-Track_1 = ["X"]
-Track_2 = ["B"]
-Track_3 = ["B"]
+Track_1 = []
+Track_2 = []
+Track_3 = []
 
 # Meminta user memasukkan input bilangan
 input_1 = int(input("Masukkan bilangan pokok: "))
@@ -11,14 +11,13 @@ input_1 = int(input("Masukkan bilangan pokok: "))
 for i in range(abs(input_1)):
     Track_1.append(0)
 
-# Memberi pembatas
-Track_1.append(1)
 
 # Menambahkan blank di awal dan akhir di semua track
 for i in range(input_1 + 1):
     Track_1.append("B")
 for i in range(input_1 + 1):
     Track_1.insert(0, "B")
+Track_1.append("B")
 
 for i in range(input_1):
     Track_2.append("B")
@@ -35,13 +34,13 @@ for i in range(input_1 + 1):
 Track_3.append("B")
     
 j = input_1 + 1
-k = input_1 + 2
-l = input_1 + 2
+k = input_1 + 1
+l = input_1 + 1
 
 # Initial State q0
 q = 0
 
-while q not in [10]:
+while q not in [9]:
     print("State: q", q)
     print("Track 1: ", end="")
     for i in range (len(Track_1)):
@@ -60,94 +59,85 @@ while q not in [10]:
     print("\n")
 
     # State q0
-    if q == 0:       
-        if Track_1[j] == "X":
-            Track_1[j] = "B"
+    if q == 0:
+        if Track_1[j] == 0 and Track_2[k] == "B" and Track_3[l] == "B":
             j += 1
+        elif Track_1[j] == "B" and Track_2[k] == "B" and Track_3[l] == "B":
+            j -= 1
             q = 1
-
     # State q1
     elif q == 1:
         if Track_1[j] == 0 and Track_2[k] == "B" and Track_3[l] == "B":
-            j += 1
-            q = 1
-        elif Track_1[j] == 1 and Track_2[k] == "B" and Track_3[l] == "B":
             Track_1[j] = "B"
             j -= 1
             q = 2
+        elif Track_1[j] == "B" and Track_2[k] == "B" and Track_3[l] == "B":
+            q = 8
     # State q2
     elif q == 2:
-        if Track_1[j] == 0 and Track_2[k] == "B" and Track_3[l] == "B":
-            Track_1[j] = "B"
-            j -= 1
-            q = 3
-        elif Track_1[j] == "B" and Track_2[k] == "B" and Track_3[l] == "B":
-            q = 9
-    # State q3
-    elif q == 3:
         if Track_1[j] == 0 and Track_2[k] == "B" and Track_3[l] == "B":
             Track_1[j] = "B"
             Track_2[k] = 0
             j -= 1
             k += 1
-            q = 4
+            q = 3
         elif Track_1[j] == "B" and Track_2[k] == "B" and Track_3[l] == "B":
             l -= 1
-            q = 6
+            q = 5
 
-    # State q4
-    elif q == 4:
+    # State q3
+    elif q == 3:
         if Track_1[j] == "B" and Track_2[k] == "B" and Track_3[l] == "B":
             Track_3[l] = 0
             k -= 1
             l += 1
-            q = 5
+            q = 4
         elif Track_1[j] == 0 and Track_2[k] == "B" and Track_3[l] == "B":
-            q = 2
-    # State q5
-    elif q == 5:
+            q = 1
+    # State q4
+    elif q == 4:
         if Track_1[j] == "B" and Track_2[k] == 0 and Track_3[l] == "B":
             Track_1[j] = 0
             Track_2[k] = "B"
             j += 1
             k -= 1
-            q = 5
+            q = 4
         elif Track_1[j] == "B" and Track_2[k] == "B" and Track_3[l] == "B":
             j -= 1
-            q = 2
+            q = 1
 
-    # State q6
-    elif q == 6:
+    # State q5
+    elif q == 5:
         if Track_1[j] == "B" and Track_2[k] == "B" and Track_3[l] == "B":
             l += 1
-            q = 9
+            q = 8
         elif Track_1[j] == "B" and Track_2[k] == "B" and Track_3[l] == 0:
             k -= 1
             l += 1
-            q = 7
-    # State q7
-    elif q == 7:
+            q = 6
+    # State q6
+    elif q == 6:
         if Track_1[j] == "B" and Track_2[k] == 0 and Track_3[l] == "B":
             Track_2[k] = "B"
             l -= 1
-            q = 8
+            q = 7
         elif Track_1[j] == "B" and Track_2[k] == "B" and Track_3[l] == "B":
-            q = 10
-    # State q8
-    elif q == 8:
-        if Track_1[j] == "B" and Track_2[k] == "B" and Track_3[l] == 0:
-            Track_3[l] = "B"
-            l -= 1
-            q = 8
-        elif Track_1[j] == "B" and Track_2[k] == "B" and Track_3[l] == "B":
-            l += 1
             q = 9
     # State q9
     elif q == 9:
+        if Track_1[j] == "B" and Track_2[k] == "B" and Track_3[l] == 0:
+            Track_3[l] = "B"
+            l -= 1
+            q = 7
+        elif Track_1[j] == "B" and Track_2[k] == "B" and Track_3[l] == "B":
+            l += 1
+            q = 8
+    # State q9
+    elif q == 8:
         if Track_1[j] == "B" and Track_2[k] == "B" and Track_3[l] == "B":
             Track_3[l] = "X"
             l += 1
-            q = 10
+            q = 9
 
 print("Track 1: ", end="")
 for i in range (len(Track_1)):
@@ -163,5 +153,7 @@ for i in range (len(Track_3)):
 
 a = Track_3.count(0)
 
-print("\nHasil: ", a, sep="")
-
+if a > 0:
+    print("\nHasil: ", a, sep="")
+else:
+    print("\nHasil: Input Tidak Valid")
