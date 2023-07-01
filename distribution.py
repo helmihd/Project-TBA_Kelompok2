5efba1ae4bcb594de01cd9f3199b1ef4eaf6d144
Haqqi <haqqisetiadjie@student.uns.ac.id>
2023-07-01 20:49:12 +0700

    root

--- distribution.py
+++ distribution.py
@@ -3,40 +3,89 @@ def distribution (Track_1, Track_1_arrow, Track_2, Track_2_arrow, Track_3, Track
     if q == 0:
         if Track_1[j] == "+":
             Track_2[k] = Track_1[j]
+            
+            Track_1_arrow[j] = " "
             j += 1
+            Track_1_arrow[j] = "^"
+
+            Track_2_arrow[k] = " "
             k += 1
+            Track_2_arrow[k] = "^"
+
             q = 0
         elif Track_1[j] == "-":
             Track_2[k] = Track_1[j]
+            
+            Track_1_arrow[j] = " "
             j += 1
+            Track_1_arrow[j] = "^"
+
+            Track_2_arrow[k] = " "
             k += 1
+            Track_2_arrow[k] = "^"
+
             q = 0
         elif Track_1[j] == 1 and Track_2[k] == "B" and Track_3[l] == "B":
             Track_2[k] = 1
+            
+            Track_1_arrow[j] = " "
             j += 1
+            Track_1_arrow[j] = "^"
+
+            Track_2_arrow[k] = " "
             k -= 1
+            Track_2_arrow[k] = "^"
+
             q = 1
     # State q1
     elif q == 1:
         if Track_1[j] == "+" and Track_2[k] == "+" and Track_3[l] == "B":
             Track_1[j] = "X"
+            
+            Track_1_arrow[j] = " "
             j += 1
+            Track_1_arrow[j] = "^"
+
+            Track_2_arrow[k] = " "
             k -= 1
+            Track_2_arrow[k] = "^"
+
             q = 2
         elif Track_1[j] == "-" and Track_2[k] == "-" and Track_3[l] == "B":
             Track_1[j] = "X"
+            
+            Track_1_arrow[j] = " "
             j += 1
+            Track_1_arrow[j] = "^"
+
+            Track_2_arrow[k] = " "
             k -= 1
+            Track_2_arrow[k] = "^"
+
             q = 2
         elif Track_1[j] == "-" and Track_2[k] == "+" and Track_3[l] == "B":
             Track_1[j] = "X"
+            
+            Track_1_arrow[j] = " "
             j += 1
+            Track_1_arrow[j] = "^"
+
+            Track_2_arrow[k] = " "
             k -= 1
+            Track_2_arrow[k] = "^"
+
             q = 5
         elif Track_1[j] == "+" and Track_2[k] == "-" and Track_3[l] == "B":
             Track_1[j] = "X"
+            
+            Track_1_arrow[j] = " "
             j += 1
+            Track_1_arrow[j] = "^"
+
+            Track_2_arrow[k] = " "
             k -= 1
+            Track_2_arrow[k] = "^"
+
             q = 5
         elif Track_1[j] == "B" and Track_2[k] == "B" and Track_3[l] == "B":
             q = 8
@@ -44,28 +93,60 @@ def distribution (Track_1, Track_1_arrow, Track_2, Track_2_arrow, Track_3, Track
     elif q == 2:
         if Track_1[j] == "-" and Track_2[k] == "-" and Track_3[l] == "B":
             Track_1[j] = "X"
+            
+            Track_1_arrow[j] = " "
             j += 1
+            Track_1_arrow[j] = "^"
+
+            Track_2_arrow[k] = " "
             k -= 1
+            Track_2_arrow[k] = "^"
+
             q = 2
         elif Track_1[j] == "+" and Track_2[k] == "+" and Track_3[l] == "B":
             Track_1[j] = "X"
+            
+            Track_1_arrow[j] = " "
             j += 1
+            Track_1_arrow[j] = "^"
+
+            Track_2_arrow[k] = " "
             k -= 1
+            Track_2_arrow[k] = "^"
+
             q = 2
         elif Track_1[j] == "-" and Track_2[k] == "B" and Track_3[l] == "B":
             Track_3[l] = "+"
+            Track_2_arrow[k] = " "
             k += 1
+            Track_2_arrow[k] = "^"
+
+            Track_3_arrow[l] = " "
             l += 1
+            Track_3_arrow[l] = "^"
+
             q = 3
         elif Track_1[j] == "B" and Track_2[k] == "B" and Track_3[l] == "B":
             Track_3[l] = "+"
+            Track_2_arrow[k] = " "
             k += 1
+            Track_2_arrow[k] = "^"
+
+            Track_3_arrow[l] = " "
             l += 1
+            Track_3_arrow[l] = "^"
+
             q = 3
         elif Track_1[j] == "+" and Track_2[k] == "B" and Track_3[l] == "B":
             Track_3[l] = "+"
+            Track_2_arrow[k] = " "
             k += 1
+            Track_2_arrow[k] = "^"
+
+            Track_3_arrow[l] = " "
             l += 1
+            Track_3_arrow[l] = "^"
+
             q = 3
         elif Track_1[j] == "B" and Track_2[k] == "+" and Track_3[l] == "B":
             q = 4
@@ -74,52 +155,105 @@ def distribution (Track_1, Track_1_arrow, Track_2, Track_2_arrow, Track_3, Track
     # State q3
     elif q == 3:
         if Track_1[j] == "B" and Track_2[k] == "-" and Track_3[l] == "B":
+            Track_2_arrow[k] = " "
             k += 1
+            Track_2_arrow[k] = "^"
+
             q = 3
         elif Track_1[j] == "-" and Track_2[k] == "-" and Track_3[l] == "B":
+            Track_2_arrow[k] = " "
             k += 1
+            Track_2_arrow[k] = "^"
+
             q = 3
         elif Track_1[j] == "+" and Track_2[k] == "+" and Track_3[l] == "B":
+            Track_2_arrow[k] = " "
             k += 1
+            Track_2_arrow[k] = "^"
+
             q = 3
         elif Track_1[j] == "B" and Track_2[k] == "+" and Track_3[l] == "B":
+            Track_2_arrow[k] = " "
             k += 1
+            Track_2_arrow[k] = "^"
+
             q = 3
         elif Track_1[j] == "+" and Track_2[k] == 1 and Track_3[l] == "B":
+            Track_2_arrow[k] = " "
             k -= 1
+            Track_2_arrow[k] = "^"
+
             q = 2
         elif Track_1[j] == "B" and Track_2[k] == 1 and Track_3[l] == "B":
+            Track_2_arrow[k] = " "
             k -= 1
+            Track_2_arrow[k] = "^"
+
             q = 2
         elif Track_1[j] == "-" and Track_2[k] == 1 and Track_3[l] == "B":
+            Track_2_arrow[k] = " "
             k -= 1
+            Track_2_arrow[k] = "^"
+
             q = 2
     # State q5
     elif q == 5:
         if Track_1[j] == "-" and Track_2[k] == "+" and Track_3[l] == "B":
             Track_1[j] = "X"
+            
+            Track_1_arrow[j] = " "
             j += 1
+            Track_1_arrow[j] = "^"
+
+            Track_2_arrow[k] = " "
             k -= 1
+            Track_2_arrow[k] = "^"
+
             q = 5
         elif Track_1[j] == "+" and Track_2[k] == "-" and Track_3[l] == "B":
             Track_1[j] = "X"
+            
+            Track_1_arrow[j] = " "
             j += 1
+            Track_1_arrow[j] = "^"
+
+            Track_2_arrow[k] = " "
             k -= 1
+            Track_2_arrow[k] = "^"
+
             q = 5
         elif Track_1[j] == "-" and Track_2[k] == "B" and Track_3[l] == "B":
             Track_3[l] = "-"
+            Track_2_arrow[k] = " "
             k += 1
+            Track_2_arrow[k] = "^"
+
+            Track_3_arrow[l] = " "
             l += 1
+            Track_3_arrow[l] = "^"
+
             q = 6
         elif Track_1[j] == "B" and Track_2[k] == "B" and Track_3[l] == "B":
             Track_3[l] = "-"
+            Track_2_arrow[k] = " "
             k += 1
+            Track_2_arrow[k] = "^"
+
+            Track_3_arrow[l] = " "
             l += 1
+            Track_3_arrow[l] = "^"
+
             q = 6
         elif Track_1[j] == "+" and Track_2[k] == "B" and Track_3[l] == "B":
             Track_3[l] = "-"
+            Track_2_arrow[k] = " "
             k += 1
+            Track_2_arrow[k] = "^"
+
+            Track_3_arrow[l] = " "
             l += 1
+            Track_3_arrow[l] = "^"
+
             q = 6
         elif Track_1[j] == "B" and Track_2[k] == "+" and Track_3[l] == "B":
             q = 7
@@ -128,25 +262,46 @@ def distribution (Track_1, Track_1_arrow, Track_2, Track_2_arrow, Track_3, Track
     # State q6
     elif q == 6:
         if Track_1[j] == "B" and Track_2[k] == "-" and Track_3[l] == "B":
+            Track_2_arrow[k] = " "
             k += 1
+            Track_2_arrow[k] = "^"
+
             q = 6
         elif Track_1[j] == "+" and Track_2[k] == "-" and Track_3[l] == "B":
+            Track_2_arrow[k] = " "
             k += 1
+            Track_2_arrow[k] = "^"
+
             q = 6
         elif Track_1[j] == "-" and Track_2[k] == "+" and Track_3[l] == "B":
+            Track_2_arrow[k] = " "
             k += 1
+            Track_2_arrow[k] = "^"
+
             q = 6
         elif Track_1[j] == "B" and Track_2[k] == "+" and Track_3[l] == "B":
+            Track_2_arrow[k] = " "
             k += 1
+            Track_2_arrow[k] = "^"
+
             q = 6
         elif Track_1[j] == "-" and Track_2[k] == 1 and Track_3[l] == "B":
+            Track_2_arrow[k] = " "
             k -= 1
+            Track_2_arrow[k] = "^"
+
             q = 5
         elif Track_1[j] == "+" and Track_2[k] == 1 and Track_3[l] == "B":
+            Track_2_arrow[k] = " "
             k -= 1
+            Track_2_arrow[k] = "^"
+
             q = 5
         elif Track_1[j] == "B" and Track_2[k] == 1 and Track_3[l] == "B":
+            Track_2_arrow[k] = " "
             k -= 1
+            Track_2_arrow[k] = "^"
+
             q = 5
 
     return Track_1, Track_2, Track_3, q, j, k, l
--- root.py
+++ root.py
@@ -1,19 +1,43 @@
-def root (Track_1, Track_2, Track_3, q, j, k, l):
+def root (Track_1, Track_1_arrow, Track_2, Track_2_arrow, Track_3, Track_3_arrow, q, j, k, l):
     # State q0
     if q == 0:
         if Track_1[j] == 0 and Track_2[k] == "B" and Track_3[l] == "B":
+            
+            Track_1_arrow[j] = " "
             j -= 1
+            Track_1_arrow[j] = "^"
+
         elif Track_1[j] == 1 and Track_2[k] == "B" and Track_3[l] == "B":
+            
+            Track_1_arrow[j] = " "
             j -= 1
+            Track_1_arrow[j] = "^"
+
+            Track_2_arrow[k] = " "
             k -= 1
+            Track_2_arrow[k] = "^"
+
+            Track_3_arrow[l] = " "
             l -= 1
+            Track_3_arrow[l] = "^"
+
             q = 1
     # State q1
     elif q == 1:
         if Track_1[j] == 0 and Track_2[k] == "B" and Track_3[l] == "B":
+            
+            Track_1_arrow[j] = " "
             j -= 1
+            Track_1_arrow[j] = "^"
+
+            Track_2_arrow[k] = " "
             k -= 1
+            Track_2_arrow[k] = "^"
+
+            Track_3_arrow[l] = " "
             l -= 1
+            Track_3_arrow[l] = "^"
+
         elif Track_1[j] == "B" and Track_2[k] == "B" and Track_3[l] == "B":
             Track_1[j] = 0
             q = 2
@@ -21,55 +45,126 @@ def root (Track_1, Track_2, Track_3, q, j, k, l):
     elif q == 2:
         if Track_1[j] == 0 and Track_2[k] == "B" and Track_3[l] == "B":
             Track_2[k] = 0
+            
+            Track_1_arrow[j] = " "
             j += 1
+            Track_1_arrow[j] = "^"
+
+            Track_2_arrow[k] = " "
             k += 1
+            Track_2_arrow[k] = "^"
+
+            Track_3_arrow[l] = " "
             l += 1
+            Track_3_arrow[l] = "^"
+
         elif Track_1[j] == 1 and Track_2[k] == "B" and Track_3[l] == "B":
+            
+            Track_1_arrow[j] = " "
             j -= 1
+            Track_1_arrow[j] = "^"
+
+            Track_2_arrow[k] = " "
             k -= 1
+            Track_2_arrow[k] = "^"
+
+            Track_3_arrow[l] = " "
             l -= 1
+            Track_3_arrow[l] = "^"
+
             q = 3
     # State q3
     elif q == 3:
         if Track_1[j] == 0 and Track_2[k] == 0 and Track_3[l] == "B":
             Track_1[j] = "X"
             Track_3[l] = 0
+            
+            Track_1_arrow[j] = " "
             j -= 1
+            Track_1_arrow[j] = "^"
+
+            Track_3_arrow[l] = " "
             l -= 1
+            Track_3_arrow[l] = "^"
+
         elif Track_1[j] == "B" and Track_2[k] == 0 and Track_3[l] == "B":
+            
+            Track_1_arrow[j] = " "
             j += 1
+            Track_1_arrow[j] = "^"
+
             q = 4
         elif Track_1[j] == 0 and Track_2[k] == "B" and Track_3[l] == "B":
+            
+            Track_1_arrow[j] = " "
             j += 1
+            Track_1_arrow[j] = "^"
+
             q = 5
     # State q4
     elif q == 4:
         if Track_1[j] == "X" and Track_2[k] == 0 and Track_3[l] == "B":
             Track_1[j] = 0
+            
+            Track_1_arrow[j] = " "
             j += 1
+            Track_1_arrow[j] = "^"
+
         elif Track_1[j] == 1 and Track_2[k] == 0 and Track_3[l] == "B":
             Track_2[k] = "B"
+            
+            Track_1_arrow[j] = " "
             j -= 1
+            Track_1_arrow[j] = "^"
+
+            Track_2_arrow[k] = " "
             k -= 1
+            Track_2_arrow[k] = "^"
+
             q = 3
     # State q5
     elif q == 5:
         if Track_1[j] == 1 and Track_2[k] == "B" and Track_3[l] == "B":
+            
+            Track_1_arrow[j] = " "
             j += 1
+            Track_1_arrow[j] = "^"
+
+            Track_2_arrow[k] = " "
             k += 1
+            Track_2_arrow[k] = "^"
+
+            Track_3_arrow[l] = " "
             l += 1
+            Track_3_arrow[l] = "^"
+
             q = 6
     # State q6
     elif q == 6:
         if Track_1[j] == 0 and Track_2[k] == "B" and Track_3[l] == 0:
             Track_3[l] = "B"
+            
+            Track_1_arrow[j] = " "
             j += 1
+            Track_1_arrow[j] = "^"
+
+            Track_3_arrow[l] = " "
             l += 1
+            Track_3_arrow[l] = "^"
+
         if Track_1[j] == 0 and Track_2[k] == "B" and Track_3[l] == "B":
+            
+            Track_1_arrow[j] = " "
             j -= 1
+            Track_1_arrow[j] = "^"
+
             q = 0
         elif Track_1[j] == "B" and Track_2[k] == "B" and Track_3[l] == "B":
+            
+            Track_1_arrow[j] = " "
             j -= 1
+            Track_1_arrow[j] = "^"
+
             q = 7
         elif Track_1[j] == "B" and Track_2[k] == "B" and Track_3[l] == 0:
             q = 8
@@ -77,7 +172,11 @@ def root (Track_1, Track_2, Track_3, q, j, k, l):
     elif q == 7:
         if Track_1[j] == 0 and Track_2[k] == "B" and Track_3[l] == "B":
             Track_1[j] == "B"
+            
+            Track_1_arrow[j] = " "
             j -= 1
+            Track_1_arrow[j] = "^"
+
         elif Track_1[j] == 1 and Track_2[k] == "B" and Track_3[l] == "B":
             q = 8
 
