30e9bce3add2e2ed7bc1be0fec9bf831ce7738f0
Haqqi <haqqisetiadjie@student.uns.ac.id>
2023-07-01 20:56:46 +0700

    logaritma

--- logarithm.py
+++ logarithm.py
@@ -1,16 +1,28 @@
-def logarithm (Track_1, Track_2, Track_3, q, j, k, l): 
+def logarithm (Track_1, Track_1_arrow, Track_2, Track_2_arrow, Track_3, Track_3_arrow, q, j, k, l): 
     # State q0
     if q == 0:
         if Track_1[j] == 0 and Track_2[k] == "B" and Track_3[l] == "B":
+            
+            Track_1_arrow[j] = " "
             j += 1
+            Track_1_arrow[j] = "^"
+
         elif Track_1[j] == "B" and Track_2[k] == "B" and Track_3[l] == "B":
+            
+            Track_1_arrow[j] = " "
             j -= 1
+            Track_1_arrow[j] = "^"
+
             q = 1
     # State q1
     elif q == 1:
         if Track_1[j] == 0 and Track_2[k] == "B" and Track_3[l] == "B":
             Track_1[j] = "B"
+            
+            Track_1_arrow[j] = " "
             j -= 1
+            Track_1_arrow[j] = "^"
+
             q = 2
         elif Track_1[j] == "B" and Track_2[k] == "B" and Track_3[l] == "B":
             q = 8
@@ -19,18 +31,34 @@ def logarithm (Track_1, Track_2, Track_3, q, j, k, l):
         if Track_1[j] == 0 and Track_2[k] == "B" and Track_3[l] == "B":
             Track_1[j] = "B"
             Track_2[k] = 0
+            
+            Track_1_arrow[j] = " "
             j -= 1
+            Track_1_arrow[j] = "^"
+
+            Track_2_arrow[k] = " "
             k += 1
+            Track_2_arrow[k] = "^"
+
             q = 3
         elif Track_1[j] == "B" and Track_2[k] == "B" and Track_3[l] == "B":
+            Track_3_arrow[l] = " "
             l -= 1
+            Track_3_arrow[l] = "^"
+
             q = 5
     # State q3
     elif q == 3:
         if Track_1[j] == "B" and Track_2[k] == "B" and Track_3[l] == "B":
             Track_3[l] = 0
+            Track_2_arrow[k] = " "
             k -= 1
+            Track_2_arrow[k] = "^"
+
+            Track_3_arrow[l] = " "
             l += 1
+            Track_3_arrow[l] = "^"
+
             q = 4
         elif Track_1[j] == 0 and Track_2[k] == "B" and Track_3[l] == "B":
             q = 1
@@ -39,26 +67,49 @@ def logarithm (Track_1, Track_2, Track_3, q, j, k, l):
         if Track_1[j] == "B" and Track_2[k] == 0 and Track_3[l] == "B":
             Track_1[j] = 0
             Track_2[k] = "B"
+            
+            Track_1_arrow[j] = " "
             j += 1
+            Track_1_arrow[j] = "^"
+
+            Track_2_arrow[k] = " "
             k -= 1
+            Track_2_arrow[k] = "^"
+
             q = 4
         elif Track_1[j] == "B" and Track_2[k] == "B" and Track_3[l] == "B":
+            
+            Track_1_arrow[j] = " "
             j -= 1
+            Track_1_arrow[j] = "^"
+
             q = 1
     # State q5
     elif q == 5:
         if Track_1[j] == "B" and Track_2[k] == "B" and Track_3[l] == "B":
+            Track_3_arrow[l] = " "
             l += 1
+            Track_3_arrow[l] = "^"
+
             q = 8
         elif Track_1[j] == "B" and Track_2[k] == "B" and Track_3[l] == 0:
+            Track_2_arrow[k] = " "
             k -= 1
+            Track_2_arrow[k] = "^"
+
+            Track_3_arrow[l] = " "
             l += 1
+            Track_3_arrow[l] = "^"
+
             q = 6
     # State q6
     elif q == 6:
         if Track_1[j] == "B" and Track_2[k] == 0 and Track_3[l] == "B":
             Track_2[k] = "B"
+            Track_3_arrow[l] = " "
             l -= 1
+            Track_3_arrow[l] = "^"
+
             q = 7
         elif Track_1[j] == "B" and Track_2[k] == "B" and Track_3[l] == "B":
             q = 9
@@ -66,16 +117,25 @@ def logarithm (Track_1, Track_2, Track_3, q, j, k, l):
     elif q == 7:
         if Track_1[j] == "B" and Track_2[k] == "B" and Track_3[l] == 0:
             Track_3[l] = "B"
+            Track_3_arrow[l] = " "
             l -= 1
+            Track_3_arrow[l] = "^"
+
             q = 7
         elif Track_1[j] == "B" and Track_2[k] == "B" and Track_3[l] == "B":
+            Track_3_arrow[l] = " "
             l += 1
+            Track_3_arrow[l] = "^"
+
             q = 8
     # State q8
     elif q == 8:
         if Track_1[j] == "B" and Track_2[k] == "B" and Track_3[l] == "B":
             Track_3[l] = "X"
+            Track_3_arrow[l] = " "
             l += 1
+            Track_3_arrow[l] = "^"
+
             q = 9
 
     
